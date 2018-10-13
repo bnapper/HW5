cc33c9452fe30cc1b35dc5ea292dbc911272f18c
bnapper <43353625+bnapper@users.noreply.github.com>
2018-10-12 23:26:53 -0700

    Create diamond-lines.js

---
+++ diamond-lines.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for (var i = 200; i <= 200 ; i = i + 20) {
+  var startX = 0;
+  var startY = 200;
+  var endX = 200;
+  var endY = 0;
+  line(0, 200, 200, 0);
+}
+  
+}
