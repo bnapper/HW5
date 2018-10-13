03aafad44f0dff4b420019d2fc9fb2f8627fb4b3
bnapper <43353625+bnapper@users.noreply.github.com>
2018-10-12 23:38:46 -0700

    Create taller-lines.js

---
+++ taller-lines.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  
+  for (var i = 10; i <= 400 ; i = i + 10) {
+  var startX = 10;
+  var startY = i;
+  var endX = i;
+  var endY = 10;
+  line(10, i, i, 10);
+}
+  
+}
