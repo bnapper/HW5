8c3431f69d6eef7a007fd6e8146e684a9c7e1af3
bnapper <43353625+bnapper@users.noreply.github.com>
2018-10-12 23:04:47 -0700

    Create horizontal-lines.js

---
+++ horizontal-lines.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  for (var i = 10; i <= 390 ; i = i + 10) {
+  var startX = 10;
+  var startY = i;
+  var endX = i;
+  var endY = 10;
+  line(10, i, 390, i);
+}
+  
+}
